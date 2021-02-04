b315ea773a393c42fb68c2a2c4aa8be57f462c8f
raciv65 <70599195+raciv65@users.noreply.github.com>
2021-02-03 20:10:43 -0600

    file with logic without conf

---
+++ Leaflet-Step-1/Static/js/app.js
@@ -0,0 +1,16 @@
+
+
+var myMap = L.map('mapid').setView([37.7749, -122.4194], 5);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+}).addTo(myMap);
+
+//End point
+var queryUrl="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
